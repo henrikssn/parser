1126b570883176314cd640ec8fa10d0f1d3cc820
Erik Henriksson <erik.henriksson@me.com>
2012-11-04 17:54:52 +0100

    Merged match.hs with chatterbot.hs and added some comments

--- chatterbot/chatterbot.hs
+++ chatterbot/chatterbot.hs
@@ -26,6 +26,46 @@ splitOn lst piv = let pair = (maybe (lst, [])
 substitute :: Eq a => a -> [a] -> [a] -> [a]
 substitute wc wlist slist = (joinWith (wlist `splitOn` wc) slist)
 
+-- match: According to problem description
+-- TODO: Does only work with one wildcard and will return Nothing for more
+--       than one.
+match :: Eq a => a -> [a] -> [a] -> Maybe [a]
+match _ [] [] = Just []
+match w (a:as) (b:bs)
+    | a == b = match w as bs
+    | a == w = orElse (swm (a:as) (b:bs)) (lwm (a:as) (b:bs))
+    | otherwise = Nothing
+
+-- swm: According to description of "singleWildcardMatch"
+swm :: Eq a => [a] -> [a] -> Maybe [a]
+swm (a:as) (b:bs)
+    | equals as bs = Just [b]
+    | otherwise = Nothing
+swm _ _ = Nothing
+
+-- lwm: According to description of "longerWildcardMatch"
+lwm :: Eq a => [a] -> [a] -> Maybe [a]
+lwm [] _ = Nothing
+lwm _ [] = Nothing
+lwm as bs
+    | last as == last bs = lwm (init as) (init bs)
+    | length as == 1 && length bs > 1 = Just bs
+    | otherwise = Nothing
+
+-- equals: true if lists is equivalent, otherwise false
+equals :: Eq a => [a] -> [a] -> Bool
+equals [] [] = True
+equals [] (_:_) = False
+equals (_:_) [] = False
+equals (a:as) (b:bs)
+    | a == b = equals as bs
+    | otherwise = False
+
+-- orElse: returns first parameter if that is not Nothing, notherwise second parameter
+orElse :: Maybe a -> Maybe a -> Maybe a
+orElse Nothing  x  = x
+orElse (Just a) _  = Just a
+
 -- Test
 
 s = "abc"
