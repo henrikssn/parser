e902471dcc4730a61732b4c9eaadeaa8feada92a
Johan Förberg <johan@forberg.se>
2012-11-04 16:14:14 +0100

    Strängmangling 101

---
+++ chatterbot/chatterbot.hs
@@ -0,0 +1,29 @@
+-- Chatterbit program in Haskell
+
+import Data.List
+
+-- joinWith: join strings with a given separator character.
+joinWith :: [String] -> Char -> String
+joinWith []     _   = []
+joinWith [x]    _   = x
+joinWith (x:xs) sep = x ++ [sep] ++ (xs `joinWith` sep)
+
+-- splitWithout: like splitAt, but discards the pivot element.
+splitWithout :: String -> Int -> (String, String)
+splitWithout ""  _ = ("", "")
+splitWithout str i = (i `take` str, (i + 1) `drop` str)
+
+-- splitOn: split string on a given character.
+splitOn :: String -> Char -> [String]
+splitOn ""  _   = ""
+splitOn str piv = let pair = (maybe (str, "") 
+                                    (splitWithout str)
+                                    (piv `elemIndex` str))
+                  in [fst pair] ++ splitOn (snd pair) piv
+
+-- Test
+
+s = "abc"
+d = 'b'
+
+                
