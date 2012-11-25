2f62677888579ddf747c5ce75ad7f7b888f18315
Johan Förberg <johan@forberg.se>
2012-11-25 16:41:02 +0000

    Skrivit lite om ackord.

--- funcmusic/Twinkle.hs
+++ funcmusic/Twinkle.hs
@@ -1,6 +1,7 @@
 module Twinkle where
 import Haskore
 
+fd d n = n d v
 vol  n = n   v
 v      = [Volume 80]
 lmap f l = line (map f l)
@@ -9,4 +10,9 @@ v1 = lmap vol
     [c 5 qn, c 5 qn, g 5 qn, g 5 qn, a 5 qn, a 5 qn, g 5 hn,
      f 5 qn, f 5 qn, e 5 qn, e 5 qn, d 5 qn, d 5 qn, c 5 hn]
 
-twinkle = Instr "piano" (Tempo 3 (Phrase [Dyn SF] v1))
+--exbass = lmap (fd hn) [c 3, g 2, c 3, g 2, f 3, e 3, d 3, c 3]
+
+vv = v1 --:=: exbass
+
+twinkle = Instr "piano" (Tempo 3 (Phrase [Dyn SF] vv))
+
