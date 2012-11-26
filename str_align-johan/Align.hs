29283990d7eb748400cdb70222d9b8c4d40eb1d6
Johan Förberg <johan@forberg.se>
2012-11-26 21:00:24 +0000

    copypasta.

--- str_align-johan/Align.hs
+++ str_align-johan/Align.hs
@@ -34,7 +34,25 @@ maximaBy valf (x:xs) = foldr select [x] xs
                                    | (valf e) == (valf a)    = e : acc
                                    | otherwise               = acc
 
-type AlignmentType = (String, String)
+type Alignment = (String, String)
 
-optAlignments :: String -> String -> [AlignmentType]
+mcsLength :: Eq a => [a] -> [a] -> Int
+mcsLength xs ys = mcsLen (length xs) (length ys)
+          where
+             mcsLen i j = mcsTable !! i !! j
+
+             mcsTable   = [[mcsEntry i j | j <- [0..]] 
+                                         | i <- [0..]]
+       
+             mcsEntry :: Int -> Int -> Int
+             mcsEntry _ 0 = 0
+             mcsEntry 0 _ = 0
+             mcsEntry i j
+               | x == y    = 1 + mcsLen (i-1) (j-1)
+               | otherwise = max (mcsLen i (j-1)) (mcsLen (i-1) j)
+               where
+                  x = xs !! (i-1)
+                  y = ys !! (j-1)
+
+optAlignments :: String -> String -> [Alignment]
 optAlignments _ _ = []
