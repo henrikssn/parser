4105d1ab940c4e8e152f323e299bb81a800054fd
Johan Förberg <johan@forberg.se>
2012-12-08 13:35:38 +0000

    Löst uppg 1

--- parser-johan/Parser.hs
+++ parser-johan/Parser.hs
@@ -4,6 +4,7 @@ module Parser(module CoreParser, T, digit, digitVal, chars, letter, err,
 import Prelude hiding (return, fail)
 import Char
 import CoreParser
+
 infixl 7 -#, #- 
 
 type T a = Parser a
@@ -14,35 +15,36 @@ err message cs = error (message++" near "++cs++"\n")
 iter :: Parser a -> Parser [a]  
 iter m = m # iter m >-> cons ! return [] 
 
-cons(a, b) = a:b
+cons = uncurry (:)
 
 (-#) :: Parser a -> Parser b -> Parser b
-m -# n = error "-# not implemented"
+m -# n = m # n >-> snd
 
 (#-) :: Parser a -> Parser b -> Parser a
-m #- n = error "#- not implemented"
+m #- n = m # n >-> fst
 
 spaces :: Parser String
-spaces =  error "spaces not implemented"
+spaces = let oneSpace = char ? isSpace
+         in  oneSpace # iter oneSpace >-> cons
 
 token :: Parser a -> Parser a
 token m = m #- spaces
 
 letter :: Parser Char
-letter =  error "letter not implemented"
+letter = char ? isAlpha
 
 word :: Parser String
 word = token (letter # iter letter >-> cons)
 
 chars :: Int -> Parser String
 chars 0 = return []
-chars n = char #chars (n-1) >-> cons
+chars n = char # chars (n-1) >-> cons
 
 accept :: String -> Parser String
-accept w = (token (chars (length w))) ? (==w)
+accept w = (token . chars . length) w ? (==w)
 
 require :: String -> Parser String
-require w  = error "require not implemented"
+require w = accept w ! err ("require '" ++ w ++ "' failed")
 
 lit :: Char -> Parser Char
 lit c = token char ? (==c)
