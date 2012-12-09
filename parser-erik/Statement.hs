5d1e08dae541ca9392cddc3d603019b52919475e
Erik Henriksson <erik.henriksson@me.com>
2012-12-09 05:37:20 +0000

    Fyllekod!

--- parser-erik/Statement.hs
+++ parser-erik/Statement.hs
@@ -1,16 +1,34 @@
 module Statement(T, parse, toString, fromString, exec) where
-import Prelude hiding (return, fail)
+import Prelude hiding (return, fail, read)
 import Parser hiding (T)
 import qualified Dictionary
 import qualified Expr
 type T = Statement
 data Statement =
     Assignment String Expr.T |
-    If Expr.T Statement Statement
-    deriving Show
+    If Expr.T Statement Statement |
+    Block [Statement] |
+    Skip |
+    While Expr.T Statement |
+    Read String |
+    Write Expr.T
+    deriving Show 
 
 assignment = word #- accept ":=" # Expr.parse #- require ";" >-> buildAss
 buildAss (v, e) = Assignment v e
+
+if_ = accept "if" -# Expr.parse # require "then" -# parse #
+      accept "else" -# parse >-> (uncurry . uncurry) If
+
+block = accept "begin" -# iter parse #- require "end" >-> Block
+
+skip = accept "skip" # require ";" >-> const Skip
+
+while = accept "while" -# Expr.parse # require "do" -# parse >-> uncurry While
+
+read = accept "read" -# word #- require ";" >-> Read
+
+write = accept "write" -# Expr.parse #- require ";" >-> Write
 
 exec :: [T] -> Dictionary.T String Integer -> [Integer] -> [Integer]
 exec (If cond thenStmts elseStmts: stmts) dict input = 
@@ -18,6 +36,8 @@ exec (If cond thenStmts elseStmts: stmts) dict input =
     then exec (thenStmts: stmts) dict input
     else exec (elseStmts: stmts) dict input
 
+parseStatement = assignment ! if_ ! block ! skip ! while ! read ! write
+
 instance Parse Statement where
-  parse = error "Statement.parse not implemented"
-  toString = error "Statement.toString not implemented"
+  parse = parseStatement 
+  toString = show 
